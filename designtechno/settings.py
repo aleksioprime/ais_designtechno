9531ac574c73942f87ddb5523b473347026ac2a2
aleksioprime <57744848+aleksioprime@users.noreply.github.com>
2021-02-28 20:44:37 +0300

    edit TEMPLATE_DIR

--- designtechno/settings.py
+++ designtechno/settings.py
@@ -69,10 +69,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'designtechno.urls'
 
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates', 'templates/allauth'],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
