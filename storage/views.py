71bb0a0c903ddd883eeec3eb95f3254e38fef730
aleksioprime <57744848+aleksioprime@users.noreply.github.com>
2021-03-04 00:55:23 +0300

    Previous_page with ThingEdit

--- storage/views.py
+++ storage/views.py
@@ -32,7 +32,12 @@ class ThingEdit(authView, UpdateView):
     form_class = ThingForm
     template_name = 'things_edit.html'
     slug_field = 'id'
-    success_url = reverse_lazy('storage:things')
+    default_redirect = '/'
+    def get(self, request, *args, **kwargs):
+        request.session['previous_page'] = request.META.get('HTTP_REFERER', self.default_redirect)
+        return super().get(request, *args, **kwargs)
+    def get_success_url(self): 
+        return self.request.session['previous_page']
 
 class ThingDelete(authView, DeleteView):
     model = Thing
